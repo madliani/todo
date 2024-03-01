922e98a565a4ccf652dc72a073251a50c5c73e03
madliani <madliani@hotmail.com>
2024-03-02 00:34:24 +0400

    fix: welcome text changed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ import { RouterLink } from 'vue-router'
   <v-main class="d-flex flex-column align-center justify-center">
     <v-avatar :image="IconTodo" size="256" />
     <p class="text-h3 text-center">
-      <span class="d-none d-sm-inline">Your checklist </span>
+      <span class="d-none d-sm-inline">Your tasks </span>
       <RouterLink :to="Routes.TODO">-></RouterLink>
     </p>
   </v-main>
